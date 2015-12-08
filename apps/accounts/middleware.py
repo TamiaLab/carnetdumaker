daea13213632d5aa35e0bd3567e255903a857660
Fabien Batteix <skywodd@gmail.com>
2015-12-08 15:29:12 +0100

    Update docstring and add fixme for future revision

--- apps/accounts/middleware.py
+++ apps/accounts/middleware.py
@@ -12,9 +12,8 @@ class LastActivityDateUpdateMiddleware(object):
 
     def process_request(self, request):
         """
-        Process the request, update the last activity date of current user.
-        :param request: The incoming request
-        :return: None
+        Process the request, update the last activity date of current user if logged-in.
+        :param request: The current request instance.
         """
 
         # Only handle authenticated users
@@ -22,6 +21,7 @@ class LastActivityDateUpdateMiddleware(object):
         if current_user.is_authenticated():
 
             # Update last login IP address
+            # FIXME This generate two SQL requests per view. Maybe use update_or_create instead?
             user_profile = current_user.user_profile
             user_profile.last_activity_date = timezone.now()
             user_profile.save_no_rendering(update_fields=('last_activity_date',))
