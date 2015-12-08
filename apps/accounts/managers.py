9a4cf8d594708ef57e41113bad4c76f26f3adc13
Fabien Batteix <skywodd@gmail.com>
2015-12-08 15:13:05 +0100

    Move the get_online_users and get_active_users methods to the manager class.

--- apps/accounts/managers.py
+++ apps/accounts/managers.py
@@ -2,7 +2,12 @@
 Objects managers for the user accounts app.
 """
 
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
+from .settings import ONLINE_USER_TIME_WINDOW_SECONDS
 
 
 class UserProfileManager(models.Manager):
@@ -12,6 +17,21 @@ class UserProfileManager(models.Manager):
 
     def get_subscribers_for_newsletter(self):
         """
-        Return a queryset of all users accepting to receive the newsletter.
+        Return a queryset of all user accounts who accept to receive the newsletter.
         """
         return self.filter(accept_newsletter=True)
+
+    def get_online_users_accounts(self):
+        """
+        Return a queryset of all user accounts currently online.
+        """
+        offline_threshold = timezone.now() - datetime.timedelta(seconds=ONLINE_USER_TIME_WINDOW_SECONDS)
+        return self.filter(online_status_public=True,
+                           last_activity_date__isnull=False,
+                           last_activity_date__gt=offline_threshold)
+
+    def get_active_users_accounts(self):
+        """
+        Return a queryset of all active users.
+        """
+        return self.filter(user__is_active=True)
