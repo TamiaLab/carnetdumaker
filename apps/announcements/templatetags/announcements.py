a476d5ab56dc1545297d143e756b4945dd8e954c
Fabien Batteix <skywodd@gmail.com>
2015-12-09 11:01:39 +0100

    Rework template tags for the announcements app

--- apps/announcements/templatetags/announcements.py
+++ apps/announcements/templatetags/announcements.py
@@ -18,7 +18,7 @@ def recent_announcements(nb_objects=NB_ANNOUNCEMENTS_PER_PAGE_WIDGET):
     :param nb_objects: The maximum number of objects to be displayed.
     """
     announcement_list = Announcement.objects.published() \
-                            .select_related('author')[:nb_objects]
+                            .select_related('author').prefetch_related('tags')[:nb_objects]
     return {
         'announcements': announcement_list
     }
@@ -31,15 +31,25 @@ def recent_announcements_list(nb_objects=NB_ANNOUNCEMENTS_PER_PAGE_WIDGET):
     :param nb_objects: The maximum number of objects to be returned.
     :return: A list of all N recently published announcements.
     """
-    return Announcement.objects.published().select_related('author')[:nb_objects]
+    return Announcement.objects.published() \
+               .select_related('author').prefetch_related('tags')[:nb_objects]
 
 
 @register.inclusion_tag('announcements/announcement_list_widget.html')
 def global_announcements():
     """
-    Display a list of all published global announcements.
+    Display a list of all published site-wide announcements.
     """
-    announcement_list = Announcement.objects.published_site_wide().select_related('author')
+    announcement_list = Announcement.objects.published_site_wide() \
+        .select_related('author').prefetch_related('tags')
     return {
         'announcements': announcement_list
     }
+
+@register.assignment_tag
+def global_announcements_list():
+    """
+    Returns a list of all N published site-wide announcements.
+    """
+    return Announcement.objects.published_site_wide() \
+        .select_related('author').prefetch_related('tags').all()
