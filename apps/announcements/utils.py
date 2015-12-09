32672b6cc8cc00bd062b4c0ff98af4f6425471c8
Fabien Batteix <skywodd@gmail.com>
2015-12-09 13:01:36 +0100

    Stub twitter publisher function for the announcements app (work in progress)

---
+++ apps/announcements/utils.py
@@ -0,0 +1,14 @@
+"""
+Utilities for the announcements app.
+"""
+
+
+def publish_announcement_on_twitter(announcement):
+    """
+    Publish the given announcement on Twitter.
+    :param announcement: The announcement to be published.
+    :return: The tweet ID on success, False on error.
+    """
+    # TODO send message
+    # return publish_link_on_twitter(announcement.title, announcement.get_absolute_url())
+    return False
