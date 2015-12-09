aaab4b9b9a7072d6be8135c078d983f3623a1383
Fabien Batteix <skywodd@gmail.com>
2015-12-09 12:59:39 +0100

    Rework views of the announcements app

--- apps/announcements/views.py
+++ apps/announcements/views.py
@@ -11,7 +11,8 @@ from apps.paginator.shortcut import (update_context_for_pagination,
                                      paginate)
 
 from .settings import NB_ANNOUNCEMENTS_PER_PAGE
-from .models import Announcement
+from .models import (Announcement,
+                     AnnouncementTag)
 
 
 def announcement_list(request,
@@ -26,7 +27,8 @@ def announcement_list(request,
     """
 
     # Retrieve all published announcements
-    published_announcements = Announcement.objects.published().select_related('author')
+    published_announcements = Announcement.objects.published() \
+        .select_related('author').prefetch_related('tags')
 
     # Announcements list pagination
     paginator, page = paginate(published_announcements, request, NB_ANNOUNCEMENTS_PER_PAGE)
@@ -54,8 +56,8 @@ def announcement_detail(request, slug,
     :return: TemplateResponse
     """
 
-    # Retrieve the article
-    manager = Announcement.objects.select_related('author', 'author__user_profile')
+    # Retrieve the announcement
+    manager = Announcement.objects.select_related('author', 'author__user_profile').prefetch_related('tags')
     announcement_obj = get_object_or_404(manager, slug=slug)
 
     # Check for preview
@@ -68,6 +70,63 @@ def announcement_detail(request, slug,
         'announcement': announcement_obj,
         'title': announcement_obj.title,
     }
+    if extra_context is not None:
+        context.update(extra_context)
+
+    return TemplateResponse(request, template_name, context)
+
+
+def tag_list(request,
+             template_name='announcements/tag_list.html',
+             extra_context=None):
+    """
+    List view of all tags as a "cloud".
+    :param request: The incoming request.
+    :param template_name: The template name to be used.
+    :param extra_context: Any extra context for the template.
+    :return: TemplateResponse
+    """
+
+    # Retrieve all tag to create a freaking awesome tag cloud
+    queryset = AnnouncementTag.objects.all()
+
+    # Render the template without pagination
+    context = {
+        'title': _('Tags list'),
+        'tags': queryset,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+
+    return TemplateResponse(request, template_name, context)
+
+
+def tag_detail(request, slug,
+               template_name='announcements/tag_detail.html',
+               extra_context=None):
+    """
+    Detail view for a specific tag.
+    :param slug: The desired tag's slug.
+    :param request: The incoming request.
+    :param template_name: The template name to be used.
+    :param extra_context: Any extra context for the template.
+    :return: TemplateResponse
+    """
+
+    # Retrieve the tag
+    tag_obj = get_object_or_404(AnnouncementTag, slug=slug)
+
+    # Related announcements list pagination
+    paginator, page = paginate(tag_obj.announcements.published()
+                               .select_related('author').prefetch_related('tags'),
+                               request, NB_ANNOUNCEMENTS_PER_PAGE)
+
+    # Render the template
+    context = {
+        'title': _('Tag %s') % tag_obj.name,
+        'tag': tag_obj,
+    }
+    update_context_for_pagination(context, 'related_announcements', request, paginator, page)
     if extra_context is not None:
         context.update(extra_context)
 
