3a3cb923babfbba4234e646dc40c0a9b6364d207
Fabien Batteix <skywodd@gmail.com>
2015-12-09 12:32:26 +0100

    Add mutex to the "tweetannouncement" management command.

--- apps/announcements/management/commands/tweetannouncements.py
+++ apps/announcements/management/commands/tweetannouncements.py
@@ -4,6 +4,8 @@ Management command to cross-publish announcements on Twitter.
 
 from django.core.management.base import NoArgsCommand
 
+from apps.dbmutex import MutexLock,AlreadyLockedError, LockTimeoutError
+
 from ...models import AnnouncementTwitterCrossPublication
 
 
@@ -11,7 +13,8 @@ class Command(NoArgsCommand):
     """
     A management command which cross-publish on Twitter any pending announcements
     currently published on the site but not on Twitter yet. Simply call the ``publish_pending_announcements``of
-    the ``AnnouncementTwitterCrossPublication`` class to do the job.
+    the ``AnnouncementTwitterCrossPublication`` class to do the job. Use the ``dbmutex`` app to avoid concurrent
+    execution of the code.
     """
 
     help = "Cross-publish pending announcements on Twitter"
@@ -22,4 +25,16 @@ class Command(NoArgsCommand):
         :param options: Not used.
         :return: None.
         """
-        AnnouncementTwitterCrossPublication.objects.publish_pending_announcements()
+
+        # Lock a critical section of code
+        try:
+            with MutexLock('twitter_announcements'):
+
+                # Do the job
+                AnnouncementTwitterCrossPublication.objects.publish_pending_announcements()
+
+        except AlreadyLockedError:
+            print('Could not obtain lock (another instance of the script running?)')
+
+        except LockTimeoutError:
+            print('Task completed but the lock timed out')
