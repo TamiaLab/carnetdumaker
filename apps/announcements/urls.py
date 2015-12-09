283de65880d2a4d6f47e1fd13c0c59c2a9203f96
Fabien Batteix <skywodd@gmail.com>
2015-12-09 13:00:05 +0100

    Rework urls for the announcements app

--- apps/announcements/urls.py
+++ apps/announcements/urls.py
@@ -17,6 +17,18 @@ urlpatterns = (
     url(r'^flux/$', feeds.LatestAnnouncementsFeed(), name='latest_announcements_rss'),
     url(r'^flux/atom/$', feeds.LatestAnnouncementsAtomFeed(), name='latest_announcements_atom'),
 
+    # Tags list view
+    url(r'^tags/$', views.tag_list, name='tag_list'),
+
+    # Tag detail view
+    url(r'^tags/(?P<slug>[-a-zA-Z0-9_]+)/$', views.tag_detail, name='tag_detail'),
+
+    # Recent announcements feeds for a specific tag
+    url(r'^tags/(?P<slug>[-a-zA-Z0-9_]+)/flux/$', feeds.LatestAnnouncementsForTagFeed,
+        name='latest_tag_announcements_rss'),
+    url(r'^tags/(?P<slug>[-a-zA-Z0-9_]+)/flux/atom/$', feeds.LatestAnnouncementsForTagAtomFeed(),
+        name='latest_tag_announcements_atom'),
+
     # Announcements index page
     url(r'^(?P<slug>[-a-zA-Z0-9_]+)/$', views.announcement_detail, name='announcement_detail'),
 )
