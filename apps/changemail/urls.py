6967c8131eae3fc52d6fa8dc69c2e47d3f10abac
Fabien Batteix <skywodd@gmail.com>
2015-12-11 16:55:31 +0100

    Move changemail urls to root level to avoid nested urls conf

--- apps/changemail/urls.py
+++ apps/changemail/urls.py
@@ -8,12 +8,11 @@ from . import views
 
 
 # URL patterns configuration
-# Made to be included in the my-account namespace.
 urlpatterns = (
 
     # Step 1: request email change
     url(r'^$', views.change_email, {
-        'post_change_redirect': 'myaccount:email_change_done',
+        'post_change_redirect': 'myaccountmail:email_change_done',
         'email_template_name': 'changemail/email_change_email.txt',
         'html_email_template_name': 'changemail/email_change_email.html',
         'subject_template_name': 'changemail/email_change_subject.txt'
@@ -26,7 +25,7 @@ urlpatterns = (
     url(r'^confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<addressb64>[0-9A-Za-z_\-]+)/'
         r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.change_email_confirm, {
-            'post_confirm_redirect': 'myaccount:email_change_complete'
+            'post_confirm_redirect': 'myaccountmail:email_change_complete'
         },
         name='email_change_confirm'),
 
