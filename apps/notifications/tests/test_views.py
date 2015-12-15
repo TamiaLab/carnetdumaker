4db2a13b9b12b9dc5bcf06f826240757a0cc293f
Fabien Batteix <skywodd@gmail.com>
2015-12-15 10:53:41 +0100

    Update tests suite for notifications app views

--- apps/notifications/tests/test_views.py
+++ apps/notifications/tests/test_views.py
@@ -1,5 +1,5 @@
 """
-Test suite for the views of the notifications app.
+Tests suite for the views of the notifications app.
 """
 
 from django.test import TestCase, Client
@@ -12,7 +12,7 @@ from ..models import Notification
 
 class NotificationsViewsTestCase(TestCase):
     """
-    Test suite for the views.
+    Tests suite for the views.
     """
 
     def setUp(self):
@@ -44,33 +44,37 @@ class NotificationsViewsTestCase(TestCase):
         Test the availability of the "notifications list" view.
         """
         client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
+        client.login(username='johndoe', password='illpassword')
         response = client.get(reverse('notifications:index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'notifications/notification_list.html')
+        self.assertIn('notifications', response.context)
+        self.assertQuerysetEqual(response.context['notifications'], ['<Notification: Test 3>',
+                                                                     '<Notification: Test 1>'])
 
     def test_read_notifications_list_view_available(self):
         """
         Test the availability of the "read notifications list" view.
         """
         client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
+        client.login(username='johndoe', password='illpassword')
         response = client.get(reverse('notifications:notification_read_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'notifications/notification_list.html')
+        self.assertIn('notifications', response.context)
+        self.assertQuerysetEqual(response.context['notifications'], ['<Notification: Test 3>'])
 
     def test_unread_notifications_list_view_available(self):
         """
         Test the availability of the "unread notifications list" view.
         """
         client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
+        client.login(username='johndoe', password='illpassword')
         response = client.get(reverse('notifications:notification_unread_list'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'notifications/notification_list.html')
+        self.assertIn('notifications', response.context)
+        self.assertQuerysetEqual(response.context['notifications'], ['<Notification: Test 1>'])
 
     def test_notifications_list_view_redirect_not_login(self):
         """
@@ -81,60 +85,12 @@ class NotificationsViewsTestCase(TestCase):
         response = client.get(notifications_list_url)
         self.assertRedirects(response, '%s?next=%s' % (settings.LOGIN_URL, notifications_list_url))
 
-    def test_notifications_list_privacy(self):
-        """
-        Test the privacy of the "notifications list" view.
-        """
-        client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
-        response = client.get(reverse('notifications:index'))
-        viewable_notifications = response.context['notifications']
-        self.assertIn(self.notif1, viewable_notifications)
-        self.assertIn(self.notif3, viewable_notifications)
-        self.assertNotIn(self.notif2, viewable_notifications)
-
-    def test_notifications_list_all(self):
-        """
-        Test the result of the "notifications list" view.
-        """
-        client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
-        response = client.get(reverse('notifications:index'))
-        viewable_notifications = response.context['notifications']
-        self.assertIn(self.notif1, viewable_notifications)
-        self.assertIn(self.notif3, viewable_notifications)
-
-    def test_notifications_list_unread(self):
-        """
-        Test the result of the "unread notifications list" view.
-        """
-        client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
-        response = client.get(reverse('notifications:notification_unread_list'))
-        viewable_notifications = response.context['notifications']
-        self.assertIn(self.notif1, viewable_notifications)
-
-    def test_notifications_list_read(self):
-        """
-        Test the result of the "read notifications list" view.
-        """
-        client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
-        response = client.get(reverse('notifications:notification_read_list'))
-        viewable_notifications = response.context['notifications']
-        self.assertIn(self.notif3, viewable_notifications)
-
     def test_mark_all_notifications_as_read_view_available(self):
         """
         Test the availability of the "mark all notifications as read" view.
         """
         client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
+        client.login(username='johndoe', password='illpassword')
         response = client.get(reverse('notifications:mark_all_as_read'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'notifications/mark_all_as_read.html')
@@ -153,11 +109,12 @@ class NotificationsViewsTestCase(TestCase):
         Test the availability of the "notification detail" view.
         """
         client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johndoe', 'password': 'illpassword'})
+        client.login(username='johndoe', password='illpassword')
         response = client.get(self.notif1.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'notifications/notification_detail.html')
+        self.assertIn('notification', response.context)
+        self.assertEqual(response.context['notification'], self.notif1)
 
     def test_notification_detail_view_redirect_not_login(self):
         """
@@ -174,8 +131,26 @@ class NotificationsViewsTestCase(TestCase):
         not the recipient of the given notification.
         """
         client = Client()
-        # client.login(username='johndoe', password='illpassword')
-        client.post(settings.LOGIN_URL, {'username': 'johnsmith', 'password': 'illpassword'})
+        client.login(username='johnsmith', password='illpassword')
         response = client.get(self.notif1.get_absolute_url())
         self.assertEqual(response.status_code, 404)
         self.assertTemplateUsed(response, '404.html')
+
+    def test_my_account_view_redirect_not_login(self):
+        """
+        Test the redirection of the "my account" view when not logged-in.
+        """
+        client = Client()
+        myaccount_url = reverse('notifications:myaccount')
+        response = client.get(myaccount_url)
+        self.assertRedirects(response, '%s?next=%s' % (settings.LOGIN_URL, myaccount_url))
+
+    def test_my_account_view_available(self):
+        """
+        Test the availability of the "my account" view when logged-in.
+        """
+        client = Client()
+        client.login(username='johndoe', password='illpassword')
+        response = client.get(reverse('notifications:myaccount'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'notifications/my_account.html')
