8c0b079fe6fa7b95dce4ae9c070b8a2cd6f8dc95
Fabien Batteix <skywodd@gmail.com>
2015-12-16 16:54:54 +0100

    Add constant for "recently emailed" feature

--- apps/registration/settings.py
+++ apps/registration/settings.py
@@ -8,6 +8,9 @@ from django.conf import settings
 # Number of days before an activation link expire, three days by default.
 ACCOUNT_ACTIVATION_TIMEOUT_DAYS = getattr(settings, 'ACCOUNT_ACTIVATION_TIMEOUT_DAYS', 3)
 
+# Time window in seconds after the last mailing date during wich the activation key was "emailed recently"
+EMAIL_RECENTLY_SENT_TIME_WINDOW_SECONDS = getattr(settings, 'EMAIL_RECENTLY_SENT_TIME_WINDOW_SECONDS', 60 * 60)
+
 # Set to true to allow new user registrations (default False)
 REGISTRATION_OPEN = getattr(settings, 'REGISTRATION_OPEN', False)
 
