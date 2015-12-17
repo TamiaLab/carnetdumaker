1b3fc13caa222467de041e9634826b9f25305f77
Fabien Batteix <skywodd@gmail.com>
2015-12-17 14:18:17 +0100

    Rework setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@
 
 import os
 
+import pip
 from pip.req import parse_requirements
+session = pip.download.PipSession()
 
 from setuptools import setup, find_packages
 
@@ -17,8 +19,6 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-# TODO Include package data of module in apps
-
 # Setup config
 setup(
     name='carnetdumaker',
@@ -29,6 +29,8 @@ setup(
     license='AGPLv3',
     description='www.carnetdumaker.net website implemented with Django framework and Python 3.4',
     long_description=README,
+    author='Fabien Batteix',
+    author_email='fabien.batteix@tamialab.fr',
     url='https://github.com/TamiaLab/carnetdumaker',
     classifiers=[
         'Environment :: Web Environment',
@@ -41,6 +43,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=[str(pkg.req) for pkg in parse_requirements('requirements.txt')],
-    tests_require=[str(pkg.req) for pkg in parse_requirements('requirements-dev.txt')],
+    install_requires=[str(pkg.req) for pkg in parse_requirements('requirements.txt', session=session)],
+    tests_require=[str(pkg.req) for pkg in parse_requirements('requirements-dev.txt', session=session)],
 )
