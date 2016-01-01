4f7ba618f8438e6ceb2f480c4cbe88b12e1cafea
Fabien Batteix <skywodd@gmail.com>
2016-01-01 16:30:36 +0100

    Fix docstring

--- apps/forum/__init__.py
+++ apps/forum/__init__.py
@@ -1,7 +1,7 @@
 """
 Forum app.
 
-This reusable Django application is a simple forum with file attachments, moderation, "read" tracker and subscriptions.
+This reusable Django application is a simple forum with moderation, "read" tracker and subscriptions.
 """
 
 default_app_config = 'apps.forum.apps.ForumConfig'
