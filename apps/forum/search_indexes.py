94d459aea2dd35f9f660120b367e625e6713b208
Fabien Batteix <skywodd@gmail.com>
2016-01-01 18:53:54 +0100

    Integrate haystack into forum app

---
+++ apps/forum/search_indexes.py
@@ -0,0 +1,100 @@
+"""
+Search indexes for the forum app.
+"""
+
+from haystack import indexes
+
+from .models import (Forum,
+                     ForumThread,
+                     ForumThreadPost)
+
+
+class ForumIndex(indexes.SearchIndex, indexes.Indexable):
+    """
+    Search indexes for the ``Forum`` model.
+    """
+
+    text = indexes.CharField(document=True, use_template=True)
+
+    closed = indexes.BooleanField(model_attr='closed')
+
+    def get_model(self):
+        """
+        Return the model class for this index.
+        """
+        return Forum
+
+    def index_queryset(self, using=None):
+        """
+        Used when the entire index for this model is updated.
+        """
+        return self.get_model().objects.all()
+
+    def get_updated_field(self):
+        """
+        Return the field name used to filter out recently modified objects.
+        """
+        return 'last_modification_date'
+
+
+class ForumThreadIndex(indexes.SearchIndex, indexes.Indexable):
+    """
+    Search indexes for the ``ForumThread`` model.
+    """
+
+    text = indexes.CharField(document=True, use_template=True)
+
+    closed = indexes.BooleanField(model_attr='closed')
+
+    resolved = indexes.BooleanField(model_attr='resolved')
+
+    def get_model(self):
+        """
+        Return the model class for this index.
+        """
+        return ForumThread
+
+    def index_queryset(self, using=None):
+        """
+        Used when the entire index for this model is updated.
+        """
+        return self.get_model().objects.public_threads().select_related('parent_forum',
+                                                                        'first_post__author')
+
+    def get_updated_field(self):
+        """
+        Return the field name used to filter out recently modified objects.
+        """
+        return 'last_modification_date'
+
+
+class ForumThreadPostIndex(indexes.SearchIndex, indexes.Indexable):
+    """
+    Search indexes for the ``ForumThreadPost`` model.
+    """
+
+    text = indexes.CharField(document=True, use_template=True)
+
+    author = indexes.CharField(model_attr='author')
+
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+
+    last_content_modification_date = indexes.DateTimeField(model_attr='last_content_modification_date', null=True)
+
+    def get_model(self):
+        """
+        Return the model class for this index.
+        """
+        return ForumThreadPost
+
+    def index_queryset(self, using=None):
+        """
+        Used when the entire index for this model is updated.
+        """
+        return self.get_model().objects.public_published()
+
+    def get_updated_field(self):
+        """
+        Return the field name used to filter out recently modified objects.
+        """
+        return 'last_modification_date'
