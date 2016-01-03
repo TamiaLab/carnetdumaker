ad9354d43733251658a626fc63e1fbd497972f91
Fabien Batteix <skywodd@gmail.com>
2016-01-03 17:19:02 +0100

    Fix provision script for elasticsearch install

--- provisioning.sh
+++ provisioning.sh
@@ -130,7 +130,9 @@ apt-get install -y memcached
 apt-get install -y libmemcached-dev zlib1g-dev libssl-dev
 
 # ----- Elastic search engine
-sudo apt-get install -y default-jre elasticsearch
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+sudo apt-get update && sudo apt-get install -y default-jre elasticsearch
 sudo update-rc.d elasticsearch defaults 95 10
 
 # ----- Virtualenv provisioning
