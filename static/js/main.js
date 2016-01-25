fd5ba8981da9e653e9e821ad55a29f91dee24035
Fabien Batteix <skywodd@gmail.com>
2016-01-25 21:41:24 +0100

    Much better javascript animation with slide and fade.

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,18 @@
+/* Back to top button */
+
 $(document).ready(function () {
     $(window).scroll(function () {
+        var back_to_top = $('#back-to-top');
         if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
+            back_to_top.fadeIn();
         } else {
-            $('#back-to-top').fadeOut();
+            back_to_top.fadeOut();
         }
     });
     // scroll body to 0px on click
-    var back_to_top = $('#back-to-top')
+    var back_to_top = $('#back-to-top');
     back_to_top.click(function () {
-        $('#back-to-top').tooltip('hide');
+        back_to_top.tooltip('hide');
         $('body,html').animate({
             scrollTop: 0
         }, 800);
@@ -18,44 +21,68 @@ $(document).ready(function () {
     back_to_top.tooltip('show');
 });
 
+
+/* Beta alert */
+
 $(document).ready(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
-            $('#beta-alert').fadeOut();
+            $('#beta-alert').animate({
+                opacity: "hide",
+                height: "hide"
+            }, 400);
         } else {
-            $('#beta-alert').fadeIn();
+            $('#beta-alert').animate({
+                opacity: "show",
+                height: "show"
+            }, 400);
         }
     });
 });
+
+
+/* blog category navbar */
 
 $(document).ready(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('#blog-category-navbar-fade').fadeOut();
+            $('#blog-category-navbar-fade').animate({
+                opacity: "hide",
+                height: "hide"
+            }, 400);
         } else {
-            $('#blog-category-navbar-fade').fadeIn();
+            $('#blog-category-navbar-fade').animate({
+                opacity: "show",
+                height: "show"
+            }, 400);
         }
     });
 });
 
+
+/* Spoiler box */
+
 $(".spoiler").each(function(index) {
-	var t = $(this);
-	t.hide();
-	var l = $('<div><input type="button" value="Voir le texte caché"></div>');
-	l.insertBefore($(this));
-	l.click(function() {
-		t.show();
-		$(this).hide();
-	});
+    var t = $(this);
+    t.hide();
+    var l = $('<div><input type="button" value="Voir le texte caché"></div>');
+    l.insertBefore($(this));
+    l.click(function() {
+        t.show();
+        $(this).hide();
+    });
 });
 
+
+/* Spoiler inline */
+
 $(".ispoiler").each(function(index) {
-	var t = $(this);
-	t.hide();
-	var l = $('<input type="button" value="Voir le texte caché">');
-	l.insertBefore($(this));
-	l.click(function() {
-		t.show();
-		$(this).hide();
-	});
+    var t = $(this);
+    t.hide();
+    var l = $('<input type="button" value="Voir le texte caché">');
+    l.insertBefore($(this));
+    l.click(function() {
+        t.show();
+        $(this).hide();
+    });
 });
