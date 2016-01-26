19513ce207da03794d3d471ab54ad369635ade47
Fabien Batteix <skywodd@gmail.com>
2016-01-26 16:52:46 +0100

    Add indexes for code snippets bundles

--- apps/snippets/search_indexes.py
+++ apps/snippets/search_indexes.py
@@ -4,7 +4,8 @@ Search indexes for the snippets app.
 
 from haystack import indexes
 
-from .models import CodeSnippet
+from .models import (CodeSnippet,
+                     CodeSnippetBundle)
 
 
 class CodeSnippetIndex(indexes.SearchIndex, indexes.Indexable):
@@ -16,6 +17,8 @@ class CodeSnippetIndex(indexes.SearchIndex, indexes.Indexable):
 
     license = indexes.CharField(model_attr='license', null=True)
 
+    author = indexes.CharField(model_attr='author', null=True)
+
     def get_model(self):
         """
         Return the model class for this index.
@@ -26,7 +29,35 @@ class CodeSnippetIndex(indexes.SearchIndex, indexes.Indexable):
         """
         Used when the entire index for this model is updated.
         """
-        return self.get_model().objects.all()
+        return self.get_model().objects.all().select_related('author', 'license')
+
+    def get_updated_field(self):
+        """
+        Return the field name used to filter out recently modified objects.
+        """
+        return 'last_modification_date'
+
+
+class CodeSnippetBundleIndex(indexes.SearchIndex, indexes.Indexable):
+    """
+    Search indexes for the ``CodeSnippetBundle`` model.
+    """
+
+    text = indexes.CharField(document=True, use_template=True)
+
+    author = indexes.CharField(model_attr='author', null=True)
+
+    def get_model(self):
+        """
+        Return the model class for this index.
+        """
+        return CodeSnippetBundle
+
+    def index_queryset(self, using=None):
+        """
+        Used when the entire index for this model is updated.
+        """
+        return self.get_model().objects.all().select_related('author')
 
     def get_updated_field(self):
         """
